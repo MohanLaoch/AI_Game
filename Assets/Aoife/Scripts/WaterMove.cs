e2bcaf9795173d2436384ed7c717027a5b0e15f5
AoifeBerry <71941986+AoifeBerry@users.noreply.github.com>
2022-05-04 00:47:18 +0100

    water

---
+++ Assets/Aoife/Scripts/WaterMove.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterMove : MonoBehaviour
+{
+
+    public float Scrollx = 0.5f;
+    public float Scrolly = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float OffsetX = Time.time * Scrollx;
+        float OffsetY = Time.time * Scrolly;
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(OffsetX, OffsetY);
+    }
+}
