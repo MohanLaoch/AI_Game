40bf417e95fa83d6287967b2088a474d936a72ec
James Mohan <c20305011@mytudublin.ie>
2022-05-17 19:48:09 +0100

    Update EnemyStats

--- Assets/Scripts/EnemyStats.cs
+++ Assets/Scripts/EnemyStats.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EnemyStats : MonoBehaviour
 {
+    public HealthBar healthbar;
     public int MaxHP;
     public int CurrentHP;
     public int MaxDamage;
@@ -29,5 +30,7 @@ public class EnemyStats : MonoBehaviour
     public void TakeDamage(int damage)
     {
         CurrentHP -= damage;
+
+        healthbar.SetHealth(CurrentHP);
     }
 }
